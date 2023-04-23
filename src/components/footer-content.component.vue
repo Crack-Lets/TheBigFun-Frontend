d40f3b342b5aea21a468cac7e4cef4868413ccab
RauVR <u201714049@upc.edu.pe>
2023-04-23 11:41:59 -0500

    feat: Footer and navbar component created

---
+++ src/components/footer-content.component.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "footer-content.component"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/navbar-content.component.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "navbar-content.component"
+}
+</script>
+
+<style scoped>
+
+</style>
