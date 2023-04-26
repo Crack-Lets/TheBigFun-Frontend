d9676963130486c14c0a09daa15fd4f68e397ed5
Isabella-SH <sorianoisabella28@gmail.com>
2023-04-26 11:46:36 -0500

    feat: About us version 1

--- src/components/aboutus-content.component.vue
+++ src/components/aboutus-content.component.vue
@@ -1,5 +1,23 @@
 <template>
-
+    <div class="card flex align-items-center justify-content-center">
+        <pv-card style="width: 25em">
+            <template #header>
+                <img alt="user header" src="https://primefaces.org/cdn/primevue/images/usercard.png" />
+            </template>
+            <template #title> Nosotros </template>
+            <template #subtitle>  </template>
+            <template #content>
+                <p>
+                    The big fun, es una plataforma de eventos que permite a
+                    sus usuarios encontrar los eventos que estén cerca a ellos,
+                    además de que les ofrece comprar entradas para los eventos
+                    que más les interesen en un solo lugar. Además de que, podra
+                    ofrecerle a sus usuarios recomendaciones de acuerdo
+                    a sus preferencias de tipos de eventos.
+                </p>
+            </template>
+        </pv-card>
+    </div>
 </template>
 
 <script>
@@ -8,6 +26,6 @@ export default {
 }
 </script>
 
-<style scoped>
 
+<style scoped>
 </style>
