4c3a15d8dcd29ddea115ebee175b544e4a151a22
S1NGHAM <u201410580@upc.edu.pe>
2023-04-28 20:15:41 -0500

    feat: buy tickets component implemented

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <navbar-content></navbar-content>
+    <div><navbar-content></navbar-content></div>
+    <div><buy-tickets-content></buy-tickets-content></div>
+    <div><footer-content class="footer"></footer-content></div>
 
 
 
@@ -8,15 +10,18 @@
 
 
 
-  <footer-content class="footer"></footer-content>
+
+
 </template>
 
 
 <script>
 import NavbarContent from "./components/navbar-content.component.vue";
 import FooterContent from "./components/footer-content.component.vue";
+import BuyTicketsContent from "@/components/buytickets-content.component.vue";
+
 export default {
-  components: {FooterContent, NavbarContent}
+  components: {BuyTicketsContent, FooterContent, NavbarContent}
 }
 </script>
 
---
+++ src/components/buytickets-content.component.vue
@@ -0,0 +1,104 @@
+<template>
+    <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
+    <div class="background">
+        <div class="title">
+            <h1>COMPRA DE TICKET</h1>
+        </div>
+        <div class="imgContainer">
+            <img :src="eventImg" alt="user picture" aria-label="Assistant Image" style="margin-left: 10px"/>
+        </div>
+        <br>
+        <div class="datatable" >
+            <pv-datatable class= "tablex" :value="tableData" style="border: 3px solid rgba(3, 83, 151, 1)"  >
+                <pv-column field="date" header="Fecha"></pv-column>
+                <pv-column field="hour" header="Hora"></pv-column>
+                <pv-column field="nameEvent" header="Nombre del evento" ></pv-column>
+                <pv-column field="qTikects" header="Cantidad tickets" ></pv-column>
+                <pv-column field="price" header="Costo" ></pv-column>
+                <pv-column field="capacity" header="Aforo"></pv-column>
+                <pv-column field="disponibility" header ="Disponibilidad"></pv-column>
+            </pv-datatable>
+        </div>
+
+
+
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "buy-tickets-content",
+    data(){
+        return{
+            eventImg: "https://www.anayainfantilyjuvenil.com/images/libros/grande/9788469833728-la-vida-es-sueno-clasicos-hispanicos.jpg",
+            tableData:[
+                {
+                    date: "25/04/2023",
+                    hour:"8:00pm",
+                    nameEvent:"Obra teatral 'La vida es un sueño'",
+                    qTikects: "2",
+                    price: "25",
+                    capacity: "200",
+                    disponibility: "83",
+                    selectedQuantity: null,
+                    quantitys:[
+                          {quantity: 1},
+                          {quantity: 2},
+                          {quantity: 3},
+                          {quantity: 4},
+                          {quantity: 5},
+                    ]
+                }
+
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.title h1 {
+    font-family: "Nunito", sans-serif;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 42px;
+    line-height: 57px;
+    color: rgba(83, 16, 92, 0.96);
+    text-align: left;
+    margin-top: 5px;
+}
+
+.background {
+    background: rgba(99, 99, 163, 0.35);
+    border-radius: 30px;
+    padding: 30px;
+}
+img{
+    display: inline;
+    width: 300px;
+    margin-right: 100px;
+}
+.imgContainer{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left:70px;
+}
+.datatable pv-datatable{
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+}
+
+@media (min-width: 768px) {
+    .background {
+        max-width: 1000px;
+        margin: 0 auto;
+        padding: 50px;
+    }
+}
+</style>
